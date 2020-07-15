4b4fcb11ee6108f49b460feb2ae203c7340685d2
Giles Smith <clintphillips@Giles-MBP.home>
2020-07-14 20:08:09 -0400

    nav styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,12 @@ import "../App.css";
 function Nav() {
   return (
     <div className="nav">
-      <ul>
-      </ul>
+      {/* <ul className="navList">
       <li>Home</li>
       <li>About</li>
       <li>Contact</li>
       <li>Blog</li>
-
+      </ul> */}
     </div>
   );
 }
